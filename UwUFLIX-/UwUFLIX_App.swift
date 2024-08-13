1c7b4b8daba7057632d6ad830da58232bd0c7297
NotMikkNick <marwinsnewsletter@gmail.com>
2024-08-14 00:54:33 +0300

    Initial Commit

---
+++ UwUFLIX-/UwUFLIX_App.swift
@@ -0,0 +1,17 @@
+//
+//  UwUFLIX_App.swift
+//  UwUFLIX-
+//
+//  Created by Marwin on 14.08.24.
+//
+
+import SwiftUI
+
+@main
+struct UwUFLIX_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
