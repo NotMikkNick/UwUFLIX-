4dbdf5c9d7dd93eac66ef143836ec73ed99dba16
NotMikkNick <marwinsnewsletter@gmail.com>
2024-08-14 13:56:15 +0300

    fixed font bugs and added a broken film "Menu" added test code MIGHT BE UNUSED( basically only Contentview Operates SMTH)

---
+++ UwUFLIX-/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  UwUFLIX-
+//
+//  Created by Marwin on 14.08.24.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
